19a702a5bcd7ea17bf57366411bb042748800984
Nazar Dia <nazar.dia@gmail.com>
2024-01-13 15:04:32 +0100

    add-spinner

--- src/main.js
+++ src/main.js
@@ -1,39 +1,84 @@
 // https://unsplash.com/documentation#search-photos
 
 import axios from 'axios';
+import { Spinner } from 'spin.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const formEl = document.querySelector(".js-search-form");
-const listEl = document.querySelector(".js-gallery");
+const formEl = document.querySelector('.js-search-form');
+const listEl = document.querySelector('.js-gallery');
+const target = document.querySelector('.js-backdrop');
 const page = 1;
 
-formEl.addEventListener("submit", onSubmit)
+const opts = {
+  lines: 14, // The number of lines to draw
+  length: 41, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 24, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#f01919', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
+const spinner = new Spinner(opts);
+
+formEl.addEventListener('submit', onSubmit);
 
 async function onSubmit(event) {
   event.preventDefault();
-  const query = event.target.elements["user-search-query"].value.trim();
-
+  const query = event.target.elements['user-search-query'].value.trim();
+  spinnerPlay();
   if (!query) {
-    return alert("Error enter any symbols")
+    spinnerStop();
+    return iziToast.info({
+      position: 'topRight',
+      message: 'Error enter any symbols',
+    });
   }
-
 
   try {
-    const {data: {results, total}} = await getPhotos(query, page)
+    const {
+      data: { results, total },
+    } = await getPhotos(query, page);
     listEl.innerHTML = createMarkup(results);
-    alert(`We found ${total} photos`)
- 
+    iziToast.success({
+      position: 'topRight',
+      message: `We found ${total} photos`,
+    });
   } catch (error) {
-    console.log("Error");
+    console.log('Error');
+  } finally {
+    spinnerStop();
   }
-  
-
-
 }
 
-function createMarkup(arr=[]) {
-  return arr.map(photo => `<li class='gallery__item'>
+function spinnerPlay() {
+  spinner.spin(target);
+  target.classList.remove('is-hidden');
+}
+function spinnerStop() {
+  spinner.stop();
+  target.classList.add('is-hidden');
+}
+
+function createMarkup(arr = []) {
+  return arr
+    .map(
+      photo => `<li class='gallery__item'>
   <img src='${photo.urls.small}' alt='${photo.alt_description}' class='gallery-img' />
-</li>`).join("")
+</li>`
+    )
+    .join('');
 }
 
 function getPhotos(query, page) {
@@ -50,4 +95,3 @@ function getPhotos(query, page) {
     },
   });
 }
-
